c8ee8ca8daf68122c0953260741c75bea5a02581
eaboudi <eaboudi@KALI.Home>
2025-06-12 12:00:24 +0100

    save local changes before merging

--- include/Response.hpp
+++ include/Response.hpp
@@ -6,7 +6,7 @@
 /*   By: eaboudi <eaboudi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 15:31:30 by sessarhi          #+#    #+#             */
-/*   Updated: 2025/06/09 16:52:46 by eaboudi          ###   ########.fr       */
+/*   Updated: 2025/06/11 23:11:45 by eaboudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,23 @@
 #include <sstream>
 #include "ConfigData.hpp"
 #include "Request.hpp"
+#include "GetMethodResponse.hpp"
 
 #define HttpVersion "HTTP/1.1 "
 std::map<int, std::string> createErrorPhrase(void);
+
+class GetMethodResponse;
+
 class Response
 {
     public:
-        Response(int status);
+        Response(int errorCode) : ErrorCode(errorCode) {};
         Response(Request &, Server &);
         ~Response();
-
-        int GetStatus()const;
-        std::string& GetData();
-        void SetData(const std::string &data);
-        std::string BuildResponse();
+        GetMethodResponse *GET;
+        int ErrorCode;
     private:
-        int StatusCode;
-        std::map<std::string, std::string> headers;
-        static const std::map<int, std::string> ErrorPhrase;
-        std::string data;
-        std::string body;
+       
 };
 
 
