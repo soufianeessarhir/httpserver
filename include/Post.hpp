c8dd584029e6822e248b4118a00949b738506e2c
soufianeessarhir <soufianeessarhir@gmail.com>
2025-06-17 21:02:39 +0100

    start implementing post class

--- include/Post.hpp
+++ include/Post.hpp
@@ -5,14 +5,27 @@
 class Post
 {
 public:
+    enum  TransferType {
+    CONTENT_LENGTH,
+    CHUNKED
+    };
+    enum  ChunkState {
+        READING_CHUNK_SIZE,
+        READING_CHUNK_SIZE_CRLF,
+        READING_CHUNK_DATA,
+        READING_CHUNK_DATA_CRLF,
+        READING_TRAILER_HEADERS,
+        READING_FINAL_CRLF,
+        CHUNK_COMPLETE,
+        CHUNK_ERROR          
+    };
     Post(std::string &);
     ~Post();
     void ProcessChunck();
     void ProcessContentLength();
-    bool IsChuck()const;
-    void SetChunked(bool);
 private:
+    TransferType transfer_type;
+    ChunkState chunk_state;
     std::ofstream OutputFile;
-    bool is_chunk;
 };
 #endif
