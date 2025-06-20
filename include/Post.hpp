04d4ba8893da45a61f4ebde6ab483866d1ceb659
soufianeessarhir <soufianeessarhir@gmail.com>
2025-06-20 20:41:23 +0100

    post implemention in progress v2

--- include/Post.hpp
+++ include/Post.hpp
@@ -13,11 +13,8 @@ public:
     };
     enum  ChunkState {
         READING_CHUNK_SIZE,
-        READING_CHUNK_SIZE_CRLF,
         READING_CHUNK_DATA,
-        READING_CHUNK_DATA_CRLF,
         READING_TRAILER_HEADERS,
-        READING_FINAL_CRLF,
         CHUNK_COMPLETE,
         CHUNK_ERROR          
     };
@@ -25,23 +22,22 @@ public:
     ~Post();
     void ProcessChunck();
     void ProcessContentLength();
-private:
     TransferType transfer_type;
     ChunkState chunk_state;
-
-    // Chunk-specific data
+private:
+    Connection *conn;
     std::string chunk_size_buffer;
     size_t current_chunk_size;
     size_t chunk_bytes_read;
     std::string chunk_data_buffer;
-    
-    // Content-Length specific data
     size_t content_length;
     size_t content_bytes_read;
-    
-    // Common data
-    // std::string body_buffer;
     std::ofstream output_file;
     size_t max_body_size;
+
+private:
+    void ReadChunkSize();
+    void ReadChunkData();
+    void ReadTrailerHeaders();
 };
 #endif
