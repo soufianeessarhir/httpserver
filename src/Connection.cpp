876bfde8c028dbaded3ef354b3ee5de842a66a23
El Houssaine Aboudi <eaboudi@e3r9p18.1337.ma>
2025-08-06 09:25:29 +0100

    maked

--- src/Connection.cpp
+++ src/Connection.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Connection.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eaboudi <eaboudi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 15:32:49 by sessarhi          #+#    #+#             */
-/*   Updated: 2025/08/02 13:14:18 by sessarhi         ###   ########.fr       */
+/*   Updated: 2025/08/06 09:24:56 by eaboudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,12 @@ void    CheckCgiExist(Connection *conn) // add by eaboudi
         QueryString = Path.substr(Pos + 1);
         Path = Path.substr(0, Pos);
     }
+    if (conn->location->cgi.empty())
+    {
+        conn->request->SetUri(Path);
+        conn->UseCgi = false;
+        return ;
+    }
     Pos = Path.find('.');
     if (Pos != Path.npos)
     {
@@ -89,34 +95,34 @@ void    CheckCgiExist(Connection *conn) // add by eaboudi
         Pos = CheckDir.find_last_of('/');
         std::string ScriptName = CheckDir.substr(Pos + 1);
         CheckDir = CheckDir.substr(0, Pos);
-        std::string CgiDir = CGiDir;
-        if (CheckDir.compare(0, CgiDir.size(), CgiDir) == 0)
+        // std::string CgiDir = CGiDir;
+        // if (CheckDir.compare(0, CgiDir.size(), CgiDir) == 0)
+        // {
+        CheckDir += '/';
+        std::string ScriptPath = CheckDir;
+        CheckDir += ScriptName;
+        struct stat FileState;
+        if (stat(CheckDir.c_str(), &FileState) == 0)
         {
-            CheckDir += '/';
-            std::string ScriptPath = CheckDir;
-            CheckDir += ScriptName;
-            struct stat FileState;
-            if (stat(CheckDir.c_str(), &FileState) == 0)
+            if (S_ISREG(FileState.st_mode))
             {
-                if (S_ISREG(FileState.st_mode))
-                {
-                    conn->UseCgi = true;
-                    conn->CgiObj = new CGI;
-                    conn->CgiObj->Ext = Ext;
-                    conn->CgiObj->QUERY_STRING = QueryString;
-                    conn->CgiObj->SCRIPT_PATH = ScriptPath;
-                    conn->CgiObj->SCRIPT_NAME = ScriptPath + ScriptName;
-                    conn->CgiObj->PATH_INFO = PathInfo;
-                    conn->CgiObj->REMOTE_PORT = conn->port;
-                    conn->CgiObj->SERVER_PROTOCOL = conn->request->GetVersion();
-                    conn->CgiObj->REMOTE_IDENT = "webserv";
-                    conn->CgiObj->CONTENT_LENGTH = conn->request->GetHeader("content-lenght");
-                    conn->CgiObj->CONTENT_TYPE = conn->request->GetHeader("content-type");
-                    conn->request->SetUri(Path);
-                    return ;
-                }
+                conn->UseCgi = true;
+                conn->CgiObj = new CGI;
+                conn->CgiObj->Ext = Ext;
+                conn->CgiObj->QUERY_STRING = QueryString;
+                conn->CgiObj->SCRIPT_PATH = ScriptPath;
+                conn->CgiObj->SCRIPT_NAME = ScriptPath + ScriptName;
+                conn->CgiObj->PATH_INFO = PathInfo;
+                conn->CgiObj->REMOTE_PORT = conn->port;
+                conn->CgiObj->SERVER_PROTOCOL = conn->request->GetVersion();
+                conn->CgiObj->REMOTE_IDENT = "webserv";
+                conn->CgiObj->CONTENT_LENGTH = conn->request->GetHeader("content-lenght");
+                conn->CgiObj->CONTENT_TYPE = conn->request->GetHeader("content-type");
+                conn->request->SetUri(Path);
+                return ;
             }
         }
+        // }
     }
     conn->request->SetUri(Path);
     conn->UseCgi = false;
